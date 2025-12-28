ab7ec221aae737de3cba86c5f75ccd57aadfb2e3
Moges <mogesse741@gmial.com>
2025-12-28 16:29:06 +0300

    change to singular

--- server/routes/postQuestionRoute.js
+++ server/routes/postQuestionRoute.js
@@ -3,7 +3,7 @@ import postQuestion from '../controller/postQuestionController.js'
 
 const postQuestionRouter = express.Router();
 
-postQuestionRouter.post("/questions", postQuestion);
+postQuestionRouter.post("/question", postQuestion);
 
 
 export default postQuestionRouter;
