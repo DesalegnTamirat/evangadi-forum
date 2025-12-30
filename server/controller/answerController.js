657d54b22192045938cd9ecffa6df4e12de2f1bd
alemutsion182-cloud <alemutsion182@gmail.com>
2025-12-30 18:27:14 +0300

    check question exists

--- server/controller/answerController.js
+++ server/controller/answerController.js
@@ -45,25 +45,43 @@ const [answers] = await dbConnection.query(
     });
     }
 };
- 
 
 const postAnswer = async (req, res) => {
-  const { question_id, answer } = req.body;
+const { question_id, answer } = req.body;
 
 
     // validate input
-  if (!question_id || !answer) {
+if (!question_id || !answer) {
     return res.status(StatusCodes.BAD_REQUEST).json({
-      message: "question_id and answer are required",
+    message: "question_id and answer are required",
     });
 
 }
- const questionIdNum = parseInt(question_id, 10);
-  if (isNaN(questionIdNum)) {
+const questionIdNum = parseInt(question_id, 10);
+if (isNaN(questionIdNum)) {
     return res.status(StatusCodes.BAD_REQUEST).json({
-      message: "Invalid question_id",
+    message: "Invalid question_id",
     });
 }
 
-};
+try {
+    // check if question exists
+    const [question] = await dbConnection.query(
+    "SELECT questionid FROM questions WHERE questionid = ?",
+    [questionIdNum]
+    );
+
+    if (question.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+        message: "Question not found",
+    });
+    }
+}catch (error) {
+  console.error(error);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: "Internal server error",
+  });
+}
+
+}
 export { getAnswers,postAnswer };
