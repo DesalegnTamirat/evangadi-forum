8298a23a5302bbdc8d69f52f98ae8f15af0ac437
Moges <mogesse741@gmial.com>
2026-01-01 12:00:34 +0300

    .....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Register from "./Pages/Register/Register"
+import { Router } from "react-router-dom"
 function App() {
 
   return (
--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from '../../axiosConfig'
+// import {useNavigate} from 'react-router-dom'
 import { useRef } from 'react';
 const Register = () => {
     const userNameDom = useRef();
@@ -7,16 +8,31 @@ const Register = () => {
     const lastNameDom = useRef();
     const emailDom = useRef();
     const passwordDom = useRef();
+
+
+    // const navigate = useNavigate();clear
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
+        const userNameValue = userNameDom.current.value;
+        const firstNameValue = firstNameDom.current.value;
+        const lastNameValue = lastNameDom.current.value;
+        const emailValue = emailDom.current.value;
+        const passwordValue = passwordDom.current.value;
+        if(!userNameValue || !firstNameValue || !lastNameValue || !emailValue || !passwordValue){
+            alert("All fields are required");
+            return;
+        }
         try {
             await axios.post('/user/register', {
-                username: "",
-                firstName: "",
-                lastName: "",
-                email: "",
-                password: ""
+                username: userNameValue,
+                firstName: firstNameValue,
+                lastName: lastNameValue,
+                email: emailValue,
+                password: passwordValue
             });
+            alert("Registration successful! Please login.");
+            // navigate('/login');
             
         } catch (error) {
             console.log(error.response)
@@ -34,22 +50,22 @@ const Register = () => {
 <br />
 <div>
     <span>First Name : </span>
-    <input type="text" placeholder='first name'/>
+    <input type="text" placeholder='first name' ref={firstNameDom}/>
 </div>
 <br />
 <div>
     <span>Last Name : </span>
-    <input type="text" placeholder='last name'/>
+    <input type="text" placeholder='last name' ref={lastNameDom}/>
 </div>
 <br />
 <div>
     <span>Email : </span>
-    <input type="email" placeholder='email'/>
+    <input type="email" placeholder='email' ref={emailDom}/>
 </div>
 <br />
 <div>
     <span>Password : </span>
-    <input type="password" placeholder='password'/>
+    <input type="password" placeholder='password' ref={passwordDom}/>
 </div>
 
 <button type='submit'>Register</button>
