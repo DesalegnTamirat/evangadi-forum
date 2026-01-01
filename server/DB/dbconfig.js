cb664630a1036ab92a7d2667ff224beb077ac62b
appleClone <desalegntsega43@gmail.com>
2026-01-01 15:33:59 +0300

    edit .env file

--- server/DB/dbconfig.js
+++ server/DB/dbconfig.js
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 import express from "express";
 const app = express();
 const dbconnection = mysql.createPool({
-  host: "localhost",
-  user: "evangadi-admin",
-  password: "evangadi@123admin",
-  database: "evangadi_forum",
+  host: "91.204.209.29",
+  user: "birhanbz_evangadi-forum-G2",
+  password: "+0L7%6UAx[%.=L2z",
+  database: "birhanbz_evangadi_forum",
   connectionLimit: 11,
 });
 
