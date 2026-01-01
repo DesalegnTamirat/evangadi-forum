2049bb423a7c9f210ff60ebd8655cefa44e4ab27
Moges <mogesse741@gmial.com>
2026-01-01 09:46:27 +0300

    resolved

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Register from "./Pages/Register/Register";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
