3a74f703fc35fdcb7861315c1edc9ab2de96f4de
Berhan-meng <berhanumengesha3@gmail..com>
2026-01-05 11:50:33 +0300

    Ask question page added

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "../Header/Header"; // Adjust path if needed
+import Askquestion from "../../Pages/Askquestion/Askquestion.jsx"
 
 function Layout() {
   return (
@@ -13,6 +14,7 @@ function Layout() {
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<h1>Login Page</h1>} />
         <Route path="/how-it-works" element={<h1>How It Works</h1>} />
+        <Route path="/question" element={<Askquestion />} />
       </Routes>
     </>
   );
