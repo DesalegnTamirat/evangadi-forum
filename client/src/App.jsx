59ebcd865a7fae850a4e7318526b98c36c2e5e72
appleClone <desalegntsega43@gmail.com>
2026-01-08 19:01:04 +0300

    fix conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,45 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { createContext, useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import axios from "./axiosConfig";
+
+export const AppState = createContext();
 
 function App() {
+  const [user, setUser] = useState(null);
+  
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  
+  async function checkUser() {
+    if (!token) {
+      navigate("/signin");
+      return;
+    }
+
+    try {
+     const {data} = await axios.get("/user/check", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(data);
+    } catch (error) {
+      console.error(error.response);
+      localStorage.removeItem("token");
+      navigate("/signin");
+    }
+  }
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <AppState.Provider value={{ user, setUser }}>
+     <Layout/> 
+    </AppState.Provider>
   );
 }
 
---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Landing from "../../Pages/Landing/Landing";
+import SharedLayout from "./SharedLayout";
+import Home from "../../Pages/Home/Home";
+import HowItWorks from "../../Pages/HowItWorks/HowItWorks";
+import NotFound from "../../Pages/NotFound/NotFound";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import Askquestion from "../../Pages/Askquestion/Askquestion.jsx";
+
+function Layout() {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/* Root route. redirect to home if logged in */}
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Protected route for Home */}
+        <Route
+          path="home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* protected route for ask question page */}
+        <Route
+          path="ask"
+          element={
+            <ProtectedRoute>
+              <Askquestion />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Landing page for signin/signup */}
+        <Route path=":mode" element={<Landing />} />
+
+        {/* Public page */}
+        <Route path="howitworks" element={<HowItWorks />} />
+        {/* catch-all redirect for any unknown route */}
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Route>
+      <Route path="/404" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default Layout;
