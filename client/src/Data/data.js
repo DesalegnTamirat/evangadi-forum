e1d16c306c2a8bf4b37d49902647a14eb538a9cb
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2026-01-20 12:24:47 +0300

    update API_BASE_URL to match backend deployment

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "https://evangadiforum.be.birhann.com";
 // const API_BASE_URL = "http://localhost:5500";
-const API_BASE_URL = "https://evangadi-forum-desalegn.onrender.com/"
+const API_BASE_URL = "https://evangadi-forum-desalegn.onrender.com"
 
 export { API_BASE_URL };
